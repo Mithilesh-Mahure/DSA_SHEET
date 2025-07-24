6f0ac445eff24770752b7ab0431d1ec466efd576
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-24 21:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (39.28%) - LeetSync

---
+++ BACKTRACKING/890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& nums) {
+        int change = 0;
+        int five = 0, ten = 0, twenty = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 5)
+                five++;
+            else if (nums[i] == 10 && five > 0)
+                five--, ten++;
+            else if (nums[i] == 20 && ten>0 && five > 0)
+                five--, ten-- , twenty++;
+            else if (nums[i] == 20 && five > 2)
+                five--, five-- , five--;
+            else return false;
+
+        }
+        return true;
+    }
+};
