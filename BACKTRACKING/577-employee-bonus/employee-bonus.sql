6d89134e93bdd91bc0e60528fd12c9a986f104ca
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-08-06 21:38:08 +0530

    Time: 1013 ms (39.15%) | Memory: 0B (100.00%) - LeetSync

--- BACKTRACKING/577-employee-bonus/employee-bonus.sql
+++ BACKTRACKING/577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,7 @@
-SELECT emp.name,bs.bonus 
+SELECT 
+    emp.name as name, 
+    bs.bonus as bonus
 FROM Employee as emp
-LEFT JOIN Bonus as bs 
+LEFT JOIN Bonus as bs
 ON emp.empId=bs.empId
-WHERE bs.bonus<1000 OR bs.bonus is NULL;
+WHERE bonus<1000 OR bonus IS NULL;
