fce16786a7d658a14e66bb6e2801ac30d3b2c6d4
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-08-10 09:44:50 +0530

    Time: 382 ms (51.70%) | Memory: 0B (100.00%) - LeetSync

--- BACKTRACKING/181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ BACKTRACKING/181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,3 +1,5 @@
-SELECT e1.name as Employee FROM employee as e1 JOIN employee as e2 
-ON e1.managerId=e2.id
-WHERE e1.salary>e2.salary
+SELECT emp1.name AS Employee
+FROM Employee as emp1
+JOIN Employee as emp2
+ON emp1.managerId=emp2.id
+WHERE emp1.salary>emp2.salary;
