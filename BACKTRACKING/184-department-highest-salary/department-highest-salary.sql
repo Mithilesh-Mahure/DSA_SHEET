c01bcf93facdf4cba0d542b54a2cc8e5939d63dc
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-08-17 11:18:48 +0530

    Time: 578 ms (95.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ BACKTRACKING/184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,10 @@
+
+SELECT d.name AS Department , e.name AS Employee , e.salary AS Salary
+FROM Employee AS e
+LEFT JOIN department AS d 
+ON e.departmentId=d.id
+WHERE(e.departmentId,e.salary) IN (
+    SELECT departmentId,MAX(salary)
+    FROM employee
+    GROUP BY departmentId
+)
