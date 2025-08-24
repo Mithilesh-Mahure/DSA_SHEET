e402dd1d8d6b5eb06585062ac827b5fb1b47ea5a
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-08-24 10:51:42 +0530

    Time: 3 ms (51.97%) | Memory: 9.9 MB (26.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,6 +3,7 @@ public:
     bool isAnagram(string s, string t) {
         unordered_map<char,int> mp1;
         unordered_map<char,int> mp2;
+        if(s.size()!=t.size()) return false;
         for(auto x:s){
             mp1[x]++;
             
